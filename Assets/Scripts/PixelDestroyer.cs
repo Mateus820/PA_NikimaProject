66fcfc5e6a46cb7ecdb0613f7a7b5f9f6675cc58
Moésio Filho <moesiof.dev@gmail.com>
2018-11-09 02:12:18 -0300

    Bug cor resolvido; Prótotipod e partícula pós destruição pixel

--- Assets/Scripts/PixelDestroyer.cs
+++ Assets/Scripts/PixelDestroyer.cs
@@ -6,15 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class PixelDestroyer : MonoBehaviour {
 
+	// [SerializeField] private GameObject effect;
 	private void OnCollisionEnter2D(Collision2D coll) 
 	{
 		if(coll.gameObject.tag == this.gameObject.tag || this.gameObject.tag == "Untagged" )
 		{
+			// Instantiate(effect, transform.position, Quaternion.identity);
             Singleton.GetInstance.gm.pixelsDestroyed++;
             if (Singleton.GetInstance.gm.pixelsDestroyed++ >= 630)
-           {
+           	{
                 SceneManager.LoadScene(4);
-        }
+        	}
            // AudioManager.instance.Play("Crack");
 			Destroy(gameObject);		 	
 		}  
